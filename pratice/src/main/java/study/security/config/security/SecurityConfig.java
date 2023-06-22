f2f76973382ffdcf3f8d81a696e50d8bfe10d4fb
ch-yang1273 <ych1273@gmail.com>
2023-06-22 22:12:34 +0900

    Security Config 통합

--- pratice/src/main/java/study/security/config/security/SecurityConfig.java
+++ pratice/src/main/java/study/security/config/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package study.security.config.security;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
@@ -12,17 +11,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import study.security.config.security.authentication.AuthenticationBeans;
+import study.security.config.security.authorization.AuthorizationBeans;
 import study.security.config.security.form.CustomAuthenticationProvider;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Slf4j
 @RequiredArgsConstructor
 @EnableWebSecurity
-@Order(1)
+@Order(0)
 public class SecurityConfig {
+
+    private final AuthenticationBeans authenticationBeans;
+    private final AuthorizationBeans authorizationBeans;
 
     private final CustomAuthenticationProvider customAuthenticationProvider;
     private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
@@ -42,16 +47,38 @@ public class SecurityConfig {
                 .build();
     }
 
+    /**
+     * `CustomFilterSecurityInterceptor`(인가 필터)를 이미 통과했으면,기존 `FilterSecurityInterceptor`는 무시된다.
+     * *
+     * FilterSecurityInterceptor.Invoke() 내용 중...
+     * filter already applied to this request and user wants us to observe
+     * once-per-request handling, so don't re-do security checking
+     */
     @Bean
-    public SecurityFilterChain formFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain ajaxFilterChain(HttpSecurity http) throws Exception {
         http
-                // AuthorizeRequests
+                // csrf 방어 disable
+                .csrf().disable()
+
+                // AuthorizeRequests (이 설정들은 동작하지 않는다. 위의 설명 참고)
                 .authorizeRequests()
-                .antMatchers("/", "/signup", "/signup-proc").permitAll()
-                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/mypage").hasRole("USER")
                 .anyRequest().authenticated()
                 .and()
-                // 로그인
+
+                // Ajax Login 인증 필터 추가
+                .addFilterBefore(authenticationBeans.loginProcessingFilter(http), UsernamePasswordAuthenticationFilter.class)
+
+                // Custom 인가 필터 추가
+                .addFilterBefore(authorizationBeans.filterSecurityInterceptor(http), FilterSecurityInterceptor.class)
+
+                // Ajax 인가 에러 처리
+                .exceptionHandling()
+//                .authenticationEntryPoint(authenticationBeans.authenticationEntryPoint())
+//                .accessDeniedHandler(authorizationBeans.accessDeniedHandler()) // 빈을 만들어 넣어도 된다.
+                .and()
+
+                // form 로그인 페이지
                 .formLogin()
                 .loginPage("/login")
                 .defaultSuccessUrl("/")
@@ -62,13 +89,13 @@ public class SecurityConfig {
                 .loginProcessingUrl("/login-proc")
                 .permitAll()
                 .and()
-                // 로그아웃
+                // form 로그아웃 페이지
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
                 .logoutSuccessUrl("/")
                 .deleteCookies()
-                .deleteCookies("JSESSIONID");
-
+                .deleteCookies("JSESSIONID")
+        ;
         return http.build();
     }
 }
